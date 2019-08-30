e09b6ae54e2c1ce88234550aec1ad7e40db74a32
wangyunhe <wangyunhe@1218.com.cn>
2019-08-30 17:58:17 +0800

    Merge branch 'master' of D:\workspace\DOTATOOL with conflicts.

---
+++ src/main/java/MaJiang.java
@@ -0,0 +1,9 @@
+package PACKAGE_NAME;
+
+/**
+ * 描述 ：
+ * 作者 ：WYH
+ * 时间 ：2019/8/30 15:05
+ **/
+public class MaJiang {
+}
---
+++ src/main/java/card/iCard.java
@@ -0,0 +1,9 @@
+package card;
+
+/**
+ * 描述 ：
+ * 作者 ：WYH
+ * 时间 ：2019/8/30 15:52
+ **/
+public interface iCard {
+}
