59bd7ae839adb51a2d6dd6dca862284029ad4637
wangyunhe <wangyunhe@1218.com.cn>
2019-09-05 15:53:21 +0800

    优化及完成部分需求

--- src/main/java/MaJiang.java
+++ src/main/java/MaJiang.java
@@ -1,15 +1,13 @@
+import card.Card;
 import card.Type;
-import card.iCard;
+import org.jb2011.lnf.beautyeye.ch3_button.BEButtonUI;
 
-import javax.accessibility.Accessible;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.*;
 import java.util.List;
+import java.util.*;
 
 /**
  * 描述 ：
@@ -60,137 +58,91 @@ public class MaJiang {
     private JLabel l13;
     private JLabel l14;
     private JLabel tip;
+    private JButton bzhong;
+    private JLabel tipb;
+    private JLabel tipt;
+    private JLabel tipw;
+    private JLabel rl1;
+    private JLabel rl2;
+    private JLabel rl3;
+    private JLabel rl4;
+    private JLabel rl5;
+    private JLabel rl6;
+    private JLabel rl7;
+    private JLabel rl8;
+    private JLabel rl9;
+    private JLabel rl10;
+    private JLabel rl11;
+    private JLabel rl12;
+    private JLabel rl13;
+    private JLabel rl14;
+    private JLabel rtip;
+    private JLabel itip;
+    private JLabel info;
+    private JButton breset;
+    private JLabel baoTip;
+    private JLabel baoLable;
+    private JLabel explain;
+    private JButton bExit;
+    private JLabel itip2;
+    private JLabel useCardIndex;
 
-    private static List<iCard> originList = new LinkedList<>();
+    //原始牌列表
+    private List<Card> rlist;
     //放牌整理好的列表
-    private static List<iCard> list = new LinkedList<>();
+    private List<Card> list;
     //放展示页面的列表
-    private static List<JLabel> labels = new LinkedList<>();
+    private List<JLabel> labels;
+    //真实牌顺序列表
+    private List<JLabel> rLabels;
+    //真实顺序对应的映射
+    private static Map<Integer, Card> map;
+    //空牌图片
+    private ImageIcon emptyIcon;
+    //红中的文本
+    private static final String ZHONG = "红中";
+
+    MyBtnMouseListener mouseListener = new MyBtnMouseListener();
+    MyRLabelListener rLabelListener = new MyRLabelListener();
+
+    {
+        emptyIcon = new ImageIcon(Toolkit.getDefaultToolkit().
+                getImage(getClass().getResource("cardPng/empty.png")));
+        emptyIcon = new ImageIcon(emptyIcon.getImage().getScaledInstance(65, 95, Image.SCALE_DEFAULT));
+    }
 
     public MaJiang() {
         init();
-    }
-
-    public static void main(String[] args) {
-        try {
-            org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper.launchBeautyEyeLNF();
-        } catch (Exception ignored) {
+        //添加事件在此执行 避免重复
+        breset.addActionListener(e -> init());
+        bExit.addActionListener(e -> System.exit(0));
+        for (int i = 0; i < labels.size(); i++) {
+            JLabel label = labels.get(i);
+            label.addMouseListener(rLabelListener);
+            JLabel rlabel = rLabels.get(i);
+            rlabel.addMouseListener(rLabelListener);
         }
-        JFrame frame = new JFrame("majiang  by.heasy");
-        frame.setContentPane(new MaJiang().p);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.setLocationRelativeTo(null);
-
-        frame.setBounds(900, 100, 900, 1500);
-        frame.setVisible(true);
-        frame.setResizable(false);
-        frame.pack();
-    }
-
-    private void check() throws Exception {
-        if (list.size() > 14) {
-            throw new Exception("超出牌数限制！");
-        }
-    }
-
-    private void addCard(iCard card) {
-        try {
-            check();
-            list.add(card);
-            sortList();
-            originList.add(card);
-        } catch (Exception e) {
-            l1.setText(e.getMessage());
-        }
-    }
-
-    private void delCard(iCard card) {
-        try {
-            if (list.isEmpty()) return;
-            Iterator<iCard> iterator = list.iterator();
-            while (iterator.hasNext()) {
-                if (iterator.next().equals(card)) {
-                    iterator.remove();
-                    break;
-                }
-            }
-            sortList();
-        } catch (Exception e) {
-            l1.setText(e.getMessage());
-        }
-    }
-
-    private void sortList() {
-        Collections.sort(list);
-        //todo 进行牌图标的重新加载
-        int i = 0;
-        for (; i < list.size(); i++) {
-            labels.get(i).setVisible(true);
-            ImageIcon imageIcon = new ImageIcon(Toolkit.getDefaultToolkit().
-                    getImage(getClass().getResource("close.png")));
-
-            labels.get(i).setIcon(imageIcon);
-            labels.get(i).setSize(10, 20);
-            labels.get(i).setText(list.get(i).toString());
-        }
-        if (i < labels.size()) {
-            while (i < labels.size()) {
-                labels.get(i++).setVisible(false);
-            }
-        }
-    }
-
-
-    class MyBtnMouseListener extends MouseAdapter {
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            super.mouseClicked(e);
-            String text = null;
-            if (e.getSource() instanceof JButton) {
-                JButton b = (JButton) e.getSource();
-                text = b.getText();
-            }
-            if (e.getSource() instanceof JLabel) {
-                JLabel l = (JLabel) e.getSource();
-                text = l.getText();
-            }
-            Type type = null;
-            if (text == null || text.length() != 2) {
-                System.out.println("文本错误");
-                return;
-            }
-            int point = (int) text.charAt(0) - 48;
-            char t = text.charAt(1);
-            switch (t) {
-                case 'w':
-                    type = Type.WAN;
-                    break;
-                case 'b':
-                    type = Type.BING;
-                    break;
-                case 't':
-                    type = Type.TIAO;
-                    break;
-                case 'z':
-                    type = Type.ZHONG;
-                    break;
-            }
-            iCard card = new iCard(point, type);
-            if (e.getButton() == MouseEvent.BUTTON1) {
-                System.out.println("左键点击");
-                addCard(card);
-                System.out.println("添加牌：" + card);
-            } else if (e.getButton() == MouseEvent.BUTTON3) {
-                System.out.println("右键点击");
-                System.out.println("删除牌：" + card);
-                delCard(card);
-            }
-            System.out.println("调整后结果：" + list);
-        }
-    }
-
-    private void init() {
-        MyBtnMouseListener mouseListener = new MyBtnMouseListener();
+        bExit.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.red));
+        breset.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.lightBlue));
+        b1t.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.green));
+        b2t.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.green));
+        b3t.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.green));
+        b4t.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.green));
+        b5t.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.green));
+        b6t.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.green));
+        b7t.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.green));
+        b8t.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.green));
+        b9t.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.green));
+        b1w.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.red));
+        b2w.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.red));
+        b3w.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.red));
+        b4w.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.red));
+        b5w.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.red));
+        b6w.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.red));
+        b7w.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.red));
+        b8w.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.red));
+        b9w.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.red));
+        bzhong.setUI(new BEButtonUI().setNormalColor(BEButtonUI.NormalColor.red));
         b1w.addMouseListener(mouseListener);
         b2w.addMouseListener(mouseListener);
         b3w.addMouseListener(mouseListener);
@@ -218,6 +170,183 @@ public class MaJiang {
         b7t.addMouseListener(mouseListener);
         b8t.addMouseListener(mouseListener);
         b9t.addMouseListener(mouseListener);
+        bzhong.addMouseListener(mouseListener);
+        l1.setName("l1");
+        l2.setName("l2");
+        l3.setName("l3");
+        l4.setName("l4");
+        l5.setName("l5");
+        l6.setName("l6");
+        l7.setName("l7");
+        l8.setName("l8");
+        l9.setName("l9");
+        l10.setName("l10");
+        l11.setName("l11");
+        l12.setName("l12");
+        l13.setName("l13");
+        l14.setName("l14");
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            //隐藏设置按钮
+            UIManager.put("RootPane.setupButtonVisible", false);
+            org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper.launchBeautyEyeLNF();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        JFrame frame = new JFrame("视频麻将记牌  by.heasy");
+        frame.setContentPane(new MaJiang().p);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //frame.setLocationRelativeTo(null);
+        //frame.getGraphicsConfiguration().getDevice().setFullScreenWindow(frame);
+        frame.setBounds(270, 270, 1366, 768);
+        frame.setVisible(true);
+        frame.setResizable(true);
+        frame.pack();
+    }
+
+    private boolean check() {
+        if (list.size() > 13 || rlist.size() > 13) {
+            info.setText("超出牌数限制！");
+            return false;
+        }
+        return true;
+    }
+
+    private void addCard(Card card) {
+        if (!check()) return;
+        list.add(card);
+        rlist.add(card);
+        sortList();
+    }
+
+    private void delListCard(Card card) {
+        if (!list.contains(card)) {
+            info.setText("无效操作");
+            return;
+        }
+        try {
+            for (JLabel label : labels) {
+                label.setIcon(emptyIcon);
+            }
+            Iterator<Card> iterator = list.iterator();
+            while (iterator.hasNext()) {
+                if (iterator.next().equals(card)) {
+                    iterator.remove();
+                    break;
+                }
+            }
+            sortList();
+        } catch (Exception e) {
+            info.setText(e.getMessage());
+        }
+    }
+
+    //删除原始列表中的牌 不准确 随机删
+    private void delRListCard(Card card) {
+        if (!rlist.contains(card)) {
+            info.setText("无效操作");
+            return;
+        }
+        try {
+            for (JLabel rLabel : rLabels) {
+                rLabel.setIcon(emptyIcon);
+            }
+            useCardIndex.setText(String.valueOf(rlist.indexOf(card)+1));
+            Iterator<Card> rIterator = rlist.iterator();
+            while (rIterator.hasNext()) {
+                if (rIterator.next().equals(card)) {
+                    rIterator.remove();
+                    info.setText("删除牌："+card);
+                    break;
+                }
+            }
+
+        } catch (Exception e) {
+            info.setText(e.getMessage());
+        }
+    }
+
+    //进行牌图标的重新加载
+    private void sortList() {
+        Collections.sort(list);
+        int i = 0;
+        for (; i < list.size(); i++) {
+            ImageIcon imageByCard = findImageByCard(list.get(i));
+            labels.get(i).setIcon(imageByCard);
+        }
+        //剩余空位设置图片为空
+        if (i < labels.size()) {
+            while (i < labels.size()) {
+                labels.get(i++).setIcon(emptyIcon);
+            }
+        }
+        //真实顺序列表图片重载部分
+        int j = 0;
+        map.clear();
+        for (; j < rlist.size(); j++) {
+            ImageIcon imageByCard = findImageByCard(rlist.get(j));
+            rLabels.get(j).setIcon(imageByCard);
+            map.put(j, rlist.get(j));
+        }
+        if (j < rLabels.size()) {
+            while (j < rLabels.size()) {
+                rLabels.get(j++).setIcon(emptyIcon);
+            }
+        }
+    }
+
+
+    class MyBtnMouseListener extends MouseAdapter {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            super.mouseClicked(e);
+            if (!(e.getSource() instanceof JButton)) {
+                return;
+            }
+            JButton b = (JButton) e.getSource();
+            String text = b.getText();
+            if (!ZHONG.equals(text) && (text == null || text.length() != 2)) {
+                info.setText("按钮文本错误！");
+                return;
+            }
+            Card card = findCardByText(text);
+            if (e.getButton() == MouseEvent.BUTTON1) {
+                addCard(card);
+            } else if (e.getButton() == MouseEvent.BUTTON2) {
+                baoLable.setText(card.toString());
+            } else if (e.getButton() == MouseEvent.BUTTON3) {
+                delRListCard(card);
+                delListCard(card);
+            }
+        }
+    }
+
+    private void init() {
+        baoLable.setText("暂无");
+        info.setText("暂无");
+        useCardIndex.setText("*");
+        rlist = new LinkedList<>();
+        //放牌整理好的列表
+        list = new ArrayList<>();
+        //放展示页面的列表
+        labels = new LinkedList<>();
+        //真实牌顺序列表
+        rLabels = new LinkedList<>();
+        //真实顺序对应的映射
+        map = new HashMap<>(32);
+        initLists();
+        for (int i = 0; i < labels.size(); i++) {
+            JLabel label = labels.get(i);
+            label.setIcon(emptyIcon);
+            JLabel rlabel = rLabels.get(i);
+            rlabel.setIcon(emptyIcon);
+        }
+    }
+
+    private void initLists() {
         labels.add(l1);
         labels.add(l2);
         labels.add(l3);
@@ -232,9 +361,80 @@ public class MaJiang {
         labels.add(l12);
         labels.add(l13);
         labels.add(l14);
-        for (JLabel label : labels) {
-            label.addMouseListener(mouseListener);
-            label.setVisible(false);
+        rLabels.add(rl1);
+        rLabels.add(rl2);
+        rLabels.add(rl3);
+        rLabels.add(rl4);
+        rLabels.add(rl5);
+        rLabels.add(rl6);
+        rLabels.add(rl7);
+        rLabels.add(rl8);
+        rLabels.add(rl9);
+        rLabels.add(rl10);
+        rLabels.add(rl11);
+        rLabels.add(rl12);
+        rLabels.add(rl13);
+        rLabels.add(rl14);
+    }
+
+    private Card findCardByText(String text) {
+        if (ZHONG.equals(text)) return new Card(0, Type.ZHONG);
+        int point = (int) text.charAt(0) - 48;
+        char t = text.charAt(1);
+        Type type = null;
+        switch (t) {
+            case '万':
+                type = Type.WAN;
+                break;
+            case '饼':
+                type = Type.BING;
+                break;
+            case '条':
+                type = Type.TIAO;
+                break;
+        }
+        return new Card(point, type);
+    }
+
+    private ImageIcon findImageByCard(Card card) {
+        String text = card.getValue();
+        int point = (int) text.charAt(0) - 48;
+        char t = text.charAt(1);
+        String fileName = "cardPng/" + point + t + ".png";
+        ImageIcon icon = new ImageIcon(Toolkit.getDefaultToolkit().
+                getImage(getClass().getResource(fileName)));
+        //icon.setImage(icon.getImage().getScaledInstance(65, 95, Image.SCALE_DEFAULT));
+        icon = new ImageIcon(icon.getImage().getScaledInstance(65, 95, Image.SCALE_DEFAULT));
+        return icon;
+    }
+
+
+    class MyRLabelListener extends MouseAdapter {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            super.mouseClicked(e);
+            if (!(e.getSource() instanceof JLabel)) {
+                return;
+            }
+            if (e.getButton() == MouseEvent.BUTTON3) {
+                JLabel l = (JLabel) e.getSource();
+                int index = Integer.parseInt(l.getText()) - 1;
+                //点击有序列表时 名字不为null
+                if (l.getName() != null) {
+                    String name = l.getName().substring(1);
+                    Integer integer = Integer.valueOf(name);
+                    if (integer > list.size()) return;
+                    Card card = list.get(integer - 1);
+                    delRListCard(card);
+                    delListCard(card);
+                    return;
+                }
+                Card delCard = map.get(index);
+                if (delCard == null) return;
+                rlist.remove(index);
+                delListCard(delCard);
+            }
         }
     }
 }
--- src/main/java/card/Card.java
+++ src/main/java/card/Card.java
@@ -1,20 +1,20 @@
 package card;
 
 /**
- * 描述 ：
+ * 描述 ：麻将牌
  * 作者 ：WYH
  * 时间 ：2019/8/30 15:52
  **/
-public class iCard implements Comparable<iCard> {
+public final class Card implements Comparable<Card> {
     private int point;
     private Type type;
 
-    public iCard(int point, Type type) {
+    public Card(int point, Type type) {
         this.point = point;
         this.type = type;
     }
 
-    public int getPoint() {
+    public final int getPoint() {
         return point;
     }
 
@@ -22,7 +22,7 @@ public class iCard implements Comparable<iCard> {
         this.point = point;
     }
 
-    public Type getType() {
+    public final Type getType() {
         return type;
     }
 
@@ -30,9 +30,18 @@ public class iCard implements Comparable<iCard> {
         this.type = type;
     }
 
+    public String getValue() {
+        String t;
+        int code = type.getCode();
+        if (code == 0) t = "z";
+        else if (code == 1) t = "w";
+        else if (code == 2) t = "t";
+        else t = "b";
+        return point + "" + t;
+    }
 
     @Override
-    public int compareTo(iCard c2) {
+    public int compareTo(Card c2) {
         if (this.getType().getCode() > c2.getType().getCode()) {
             return 1;
         } else if (this.getType().getCode() < c2.getType().getCode()) {
@@ -49,7 +58,7 @@ public class iCard implements Comparable<iCard> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
-        iCard card = (iCard) o;
+        Card card = (Card) o;
 
         if (point != card.point) return false;
         return type.getCode() == card.type.getCode();
@@ -64,6 +73,7 @@ public class iCard implements Comparable<iCard> {
 
     @Override
     public String toString() {
+        if (point == 0) return type.getDesc();
         return point + type.getDesc();
     }
 }
